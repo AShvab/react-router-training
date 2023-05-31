e9f5db13682cb32449afe7a502f085dca48a4ef3
AShvab <115644652+AShvab@users.noreply.github.com>
2023-05-31 17:27:06 +0000

    Deploying to gh-pages from @ AShvab/react-router-training@caa570cab0da9a9a5b561c6304b23caa41131d69 🚀

---
+++ static/js/544.717aa4ef.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_router_training=self.webpackChunkreact_router_training||[]).push([[544],{544:function(i,e,t){t.r(e);var a=t(184);e.default=function(){return(0,a.jsxs)("main",{children:[(0,a.jsx)("h1",{children:"Welcome"}),(0,a.jsx)("img",{src:"https://via.placeholder.com/960x240",alt:""}),(0,a.jsx)("p",{children:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto, laboriosam placeat incidunt rem illum animi nemo quibusdam quia voluptatum voluptate."})]})}}}]);
+//# sourceMappingURL=544.717aa4ef.chunk.js.map
