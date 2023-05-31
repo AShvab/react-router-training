caa570cab0da9a9a5b561c6304b23caa41131d69
AShvab <shvabovska@gmail.com>
2023-05-31 20:26:06 +0300

    add lazy

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,30 +1,27 @@
-import About from 'pages/About/About';
-import Home from 'pages/Home/Home';
-import NotFound from 'pages/NotFound';
-import Products from 'pages/Products/Products';
-import { Route, Routes } from 'react-router-dom';
-import ProductDetails from 'pages/ProductDetails/ProductDetails';
-import Mission from './Mission/Mission';
-import Reviews from './Reviews/Reviews';
-import Team from './Team/Team';
-import SharedLayout from './SharedLayout/SharedLayout';
+import { lazy } from "react";
+import { Route, Routes } from "react-router-dom";
+import SharedLayout from "./SharedLayout/SharedLayout";
+
+const About = lazy(() => import("../pages/About/About"));
+const Home = lazy(() => import("../pages/Home/Home"));
+const ProductDetails = lazy(() => import("../pages/ProductDetails/ProductDetails"));
+const Products = lazy(() => import("../pages/Products/Products"));
+const Mission = lazy(() => import("./Mission/Mission"));
+const Team = lazy(() => import("./Team/Team"));
+const Reviews = lazy(() => import("./Reviews/Reviews"));
 
 export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<SharedLayout />}>
         <Route index element={<Home />} />
-
-        <Route path="/about" element={<About />}>
+        <Route path="about" element={<About />}>
           <Route path="mission" element={<Mission />} />
           <Route path="team" element={<Team />} />
           <Route path="reviews" element={<Reviews />} />
         </Route>
-
-        <Route path="/products" element={<Products />} />
-        <Route path="/products/:id" element={<ProductDetails />} />
-
-        <Route path="*" element={<NotFound />} />
+        <Route path="products" element={<Products />} />
+        <Route path="products/:id" element={<ProductDetails />} />
       </Route>
     </Routes>
   );
--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,26 +1,42 @@
+import { Suspense } from "react";
 import { Container, Header, Link, Logo } from "./SharedLayout.styled";
+import { Toaster } from "react-hot-toast";
 
 const { Outlet } = require("react-router-dom");
 
 const SharedLayout = () => {
-    return (
-      <Container>
-        <Header>
-          <Logo>
-            <span role="img" aria-label="computer icon">
-              💻
-            </span>{" "}
-            GoMerch Store
-          </Logo>
-          <nav>
-            <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
-            <Link to="/products">Products</Link>
-          </nav>
-        </Header>
+  return (
+    <Container>
+      <Toaster
+        position="top-right"
+        toastOptions={{
+          duration: 1500,
+          style: {
+            background: '#360404',
+            color: '#fff',
+          },
+        }}
+      />
+      <Header>
+        <Logo>
+          <span role="img" aria-label="computer icon">
+            💻
+          </span>{" "}
+          GoMerch Store
+        </Logo>
+        <nav>
+          <Link to="/" end>
+            Home
+          </Link>
+          <Link to="/about">About</Link>
+          <Link to="/products">Products</Link>
+        </nav>
+      </Header>
+      <Suspense fallback={<div>Loading page...</div>}>
         <Outlet />
-      </Container>
-    );
-  };
+      </Suspense>
+    </Container>
+  );
+};
 
   export default SharedLayout;
